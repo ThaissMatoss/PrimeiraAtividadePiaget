c2dfeb72857b219143ee92e2c4ceae62f0023613
ThaissMatoss <thaismatos2903@gmail.com>
2025-03-17 19:27:33 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiaget/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿namespace WebPiaget.Models
+{
+    public class Aluno
+    {
+        public Guid id { get; set; }
+        public string? Nome { get; set; }
+        public string?Escola { get; set; }
+        public string? Pai {get; set; }
+        public string? Mae { get; set; }
+        public string? RGM { get; set; }
+
+        public DateTime Idade { get; set; }
+
+        public decimal Notas { get; set; }
+
+        public string? Professor { get; set; }
+
+    }
+}
